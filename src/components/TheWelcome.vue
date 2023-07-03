8c54fa4518de1db6cd80cb8e298bf69e787d960c
Karmeds <carmeds@gmail.com>
2023-07-03 11:23:28 -0500

    Instalacion

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Pagina principal de Vue</h1>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>Contactanos</h1> 
+  <br/>
+    <p>Prueba</p>
+  </div>
+</template>
+
+<style>
+</style>
